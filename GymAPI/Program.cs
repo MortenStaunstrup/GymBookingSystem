d9f02bab69fd62a742f2b4cde56937dae16b2ebf
MortenStaunstrup <mort490fgromling@gmail.com>
2025-05-31 22:39:32 +0200

    init commit

---
+++ GymAPI/Program.cs
@@ -0,0 +1,40 @@
+using DotNetEnv;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("policy", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("policy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
