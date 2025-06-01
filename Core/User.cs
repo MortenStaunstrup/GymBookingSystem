f76c857d13560ab5cf4ce4bbb1cdc35f2ccab2c2
MortenStaunstrup <mort490fgromling@gmail.com>
2025-06-01 18:48:13 +0200

    Page and navbar set up

---
+++ Core/User.cs
@@ -0,0 +1,11 @@
+namespace Core;
+
+public class User
+{
+    private string Role { get; set; }
+    private string UserName { get; set; }
+    private string Password { get; set; }
+    private string Email { get; set; }
+    private string PhoneNumber { get; set; }
+    private int Age { get; set; }
+}
